06a1b334c27c8ca320b6bb312466d9cdd1643088
Matthias Beyer <mail@beyermatthias.de>
2024-01-16 09:17:47 +0100

    Update nixpkgs: 23.05 -> 23.11
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "Utility for building large cargo workspaces with crane";
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.05";
+    nixpkgs.url = "nixpkgs/nixos-23.11";
     flake-utils = {
       url = "github:numtide/flake-utils";
     };
